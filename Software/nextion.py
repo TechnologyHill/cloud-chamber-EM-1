84a8c370a814113a838e59428e463c060926334f
cesilko1 <wilhelm.master@gmail.com>
2019-09-23 23:02:39 +0200

    new library for NEXTION #5

--- Software/nextion.py
+++ Software/nextion.py
@@ -1,162 +1,113 @@
 from machine import UART
-from time import sleep_ms
+from micropython import const
+import ustruct
 
-CMD_END = b'\xFF\xFF\xFF'
+COMMAND_PAGE_ID = 'page'
+COMMAND_COMPONENT = 'ref'
+COMMAND_CLICK = 'click'
+COMMAND_STOP_REFRESH = 'ref_stop'
+COMMAND_REFRESH_START = 'ref_star'
+COMMAND_GET = 'get'
+COMMAND_GET_PAGE = 'sendme'
+COMMAND_VISIBLE = 'vis'
+COMMAND_TOUCH_ENABLED = 'tsw'
+COMMAND_ADD_DATA = 'add'
+COMMAND_ADD_DATA_BULK = 'addt'
+COMMAND_CLEAR = 'cle'
+COMMAND_RESET = 'rest'
 
-class Nextion:
-    """
-    Small library to comunicate with Nextion display
-    """
+R_COMMAND_TOUCH_EVENT = const(0x65)
+R_COMMAND_PAGE_ID = const(0x66)
+R_COMMAND_TOUCH_COORDINATE = const(0x67)
+R_COMMAND_TOUCH_COORDINATE_SLEEP_MODE = const(0x68)
+R_COMMAND_STRING_DATA = const(0x70)
+R_COMMAND_NUMERIC_DATA = const(0x71)
+R_COMMAND_AUTO_SLEEP = const(0x86)
+R_COMMAND_AUTO_WAKE_UP = const(0x87)
+R_COMMAND_SYSTEM_STARTUP = const(0x88)
+R_COMMAND_SD_UPGRADE = const(0x89)
+R_COMMAND_TRANSPARENT_TRANSMIT_FINISHED = const(0xFD)
+R_COMMAND_TRANSPARENT_TRANSMIT_READY = const(0xFE)
 
-    def __init__(self, id, baud_rate=9600):
-        """
-        Init the uart
-        """
 
-        try:
-            self.uart = UART(id, baud_rate)
-            self.uart.init(baud_rate, bits=8, parity=None, stop=1)
+class Nextion(UART):
 
-        except:
-            print("error while opening UART")
+    read_buffer = bytearray(100)
 
-    def send(self, data):
-        """
-        Send data to Nextion
-        """
+    command = None
+    page = None
+    id = None
+    touch_event = None
+    x = None
+    y = None
+    data = None
 
-        try:
-            self.response = self.uart.read()
-            self.uart.write(data)
-            self.uart.write(CMD_END)
-            sleep_ms(100)
-            self.response = self.uart.read()
-            if self.response != None:
-                return self.response
+    def __init__(self, port=1, baud=9200):
+        super(Nextion, self).__init__(port, baud)
+
+    def send(self, command, *args):
+        length = len(args)
+        for arg in args:
+            length += len(str(arg))
+        write_buffer = bytearray(len(command) + length + 3)
+        write_buffer[0: len(command)] = bytes(command, 'ASCII')
+        index = 0
+        for arg in args:
+            if index > 0:
+                write_buffer[len(command) + index:len(command) + index + 1] = b','
             else:
-                return True
+                write_buffer[len(command) + index:len(command) + index + 1] = b' '
+            #write_buffer[len(command) + index:len(command) + index + 1] = b' '
+            write_buffer[len(command) + index + 1:len(command) + index + 1 + len(str(arg))] = bytes(str(arg), 'ASCII')
+            index += 1 + len(str(arg))
+        write_buffer[-3:] = b'\xff\xff\xff'
+        print(write_buffer)
+        self.write(write_buffer)
 
-        except:
-            return False
+    def check_data(self, callback):
+        if self.any():
+            chars = self.readinto(self.read_buffer, 100)
+            index = 0
+            while index < chars:
+                self.command = self.read_buffer[index]
+                index += 1
+                if self.command in [R_COMMAND_TOUCH_EVENT, R_COMMAND_PAGE_ID]:
+                    self.page = self.read_buffer[index]
+                    index += 1
+                if self.command == R_COMMAND_TOUCH_EVENT:
+                    self.id = self.read_buffer[index]
+                    index += 1
+                if self.command in [R_COMMAND_TOUCH_COORDINATE, R_COMMAND_TOUCH_COORDINATE_SLEEP_MODE]:
+                    self.x = self.read_buffer[index:index + 2]
+                    self.y = self.read_buffer[index + 2:index + 4]
+                    index += 4
+                if self.command in [R_COMMAND_TOUCH_EVENT, R_COMMAND_TOUCH_COORDINATE, R_COMMAND_TOUCH_COORDINATE_SLEEP_MODE]:
+                    self.touch_event = self.read_buffer[index]
+                    index += 1
+                if self.command == R_COMMAND_STRING_DATA:
+                    length = 0
+                    while self.read_buffer[index + length:index + length + 3] != b'\xff\xff\xff':
+                        length += 1
+                    self.data = self.read_buffer[index:index + length]
+                    index += length
+                if self.command == R_COMMAND_NUMERIC_DATA:
+                    self.data = ustruct.unpack('<i', self.read_buffer[index:index + 4])[0]
+                    index += 4
+                if self.read_buffer[index: index + 3] == b'\xff\xff\xff':
+                    index += 3
+                    callback(command=self.command, page=self.page, id=self.id, touch_event=self.touch_event)
 
-    def page(self, data):
-        """
-        Refresh page
-        """
+                    # Reset
+                    self.command = None
+                    self.page = None
+                    self.id = None
+                    self.touch_event = None
+                    self.x = None
+                    self.y = None
+                    self.data = None
+                else:
+                    print('invalid command')
+                    return
 
-        try:
-            self.uart.write('page ')
-            self.uart.write(str(data))
-            self.uart.write(CMD_END)
-            return True
 
-        except:
-            return False
 
-    def ref(self, data):
-        """
-        Refresh component
-        """
-
-        try:
-            self.uart.write('ref ')
-            self.uart.write(data)
-            self.uart.write(CMD_END)
-            return True
-
-        except:
-            return False
-
-    def click(self, component, state):
-        """
-        Activate component's press/release event
-        """
-
-        try:
-            self.uart.write('click ')
-            self.uart.write(component)
-            self.uart.write(',')
-            self.uart.write(str(state))
-            self.uart.write(CMD_END)
-            return True
-
-        except:
-            return False
-
-    def ref_stop(self):
-        """
-        Stop refreshing screen
-        """
-
-        try:
-            self.uart.write("ref_stop")
-            self.uart.write(CMD_END)
-            return True
-        
-        except:
-            return False
-
-    def ref_star(self):
-        """
-        Stop refreshing screen
-        """
-
-        try:
-            self.uart.write("ref_star")
-            self.uart.write(CMD_END)
-            return True
-        
-        except:
-            return False
-
-    def get(self, data):
-        """
-        Get variable/constant value with format
-        """
-
-        try:
-            self.response = self.uart.read()
-            self.uart.write("get ")
-            self.uart.write(data)
-            self.uart.write(CMD_END)
-            sleep_ms(100)
-            self.response = self.uart.read()
-
-        except:
-            return False
-
-    def substr(self, text0, text1, str_start, str_length):
-        """
-        Extracts the characters from a string
-        """
-
-        try:
-            self.uart.write("substr ")
-            self.uart.write(text0)
-            self.uart.write(",")
-            self.uart.write(text1)
-            self.uart.write(",")
-            self.uart.write(str_start)
-            self.uart.write(",")
-            self.uart.write(str_start)
-            self.uart.write(CMD_END)
-            return True
-
-        except:
-            return False
-
-    def vis(self, object, state):
-        """
-        Hide/show component
-        """
-        
-        try:
-            self.uart.write("vis ")
-            self.uart.write(object)
-            self.uart.write(",")
-            self.uart.write(state)
-            self.uart.write(CMD_END)
-            return True
-
-        except:
-            return False
